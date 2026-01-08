c39e31696988dc6800b63f2d0a353f4c2b7a768e
XaquinAlves <xaquinalves@gmail.com>
2026-01-08 11:22:58 +0100

    Done Basics

--- react-test/prueba-react/src/App.jsx
+++ react-test/prueba-react/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-function App() {
-    const [count, setCount] = useState(0);
 
+function App() {
     return (
         <>
             <Navigator />
             <Carrousel />
+            <Galeria />
+            <Footer />
         </>
     );
 }
@@ -51,13 +52,14 @@ export function Example() {
                     count is {count}
                 </button>
             </div>
+            <hr className="bg-secondary" />
         </>
     );
 }
 
 export function Navigator() {
     return (
-        <div className="row mb-3 mb-sm-3">
+        <header className="row mb-3 mb-sm-3" id="inicio">
             <nav
                 className="navbar navbar-expand-lg bg-dark px-5 justify-content-between"
                 data-bs-theme="dark"
@@ -131,70 +133,156 @@ export function Navigator() {
                     </button>
                 </form>
             </nav>
-        </div>
+        </header>
     );
 }
 
 export function Carrousel() {
     return (
-        <div id="carouselExampleIndicators" className="carousel slide">
-            <div className="carousel-indicators">
-                <button
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide-to="0"
-                    className="active"
-                    aria-current="true"
-                    aria-label="Slide 1"
-                ></button>
-                <button
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide-to="1"
-                    aria-label="Slide 2"
-                ></button>
-                <button
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide-to="2"
-                    aria-label="Slide 3"
-                ></button>
-            </div>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src="img/s1.jpg" class="d-block w-100" alt="..." />
-                </div>
-                <div className="carousel-item">
-                    <img src="img/s2.jpg" class="d-block w-100" alt="..." />
-                </div>
-                <div className="carousel-item">
-                    <img src="img/s3.jpg" class="d-block w-100" alt="..." />
-                </div>
-            </div>
-            <button
-                className="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev"
+        <div className="row mb-3 mb-sm-3 mx-5 mx-sm-5">
+            <div
+                id="carouselCabecera"
+                className="carousel slide"
+                data-bs-ride="carousel"
             >
-                <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button
-                className="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next"
-            >
-                <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+                <div className="carousel-indicators">
+                    <button
+                        type="button"
+                        data-bs-target="#carouselCabecera"
+                        data-bs-slide-to="0"
+                        className="active"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselCabecera"
+                        data-bs-slide-to="1"
+                        aria-label="Slide 2"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselCabecera"
+                        data-bs-slide-to="2"
+                        aria-label="Slide 3"
+                    ></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img
+                            src="src/img/s1.jpg"
+                            class="d-block w-100"
+                            alt="..."
+                        />
+                    </div>
+                    <div className="carousel-item">
+                        <img
+                            src="src/img/s2.jpg"
+                            class="d-block w-100"
+                            alt="..."
+                        />
+                    </div>
+                    <div className="carousel-item">
+                        <img
+                            src="src/img/s3.jpg"
+                            class="d-block w-100"
+                            alt="..."
+                        />
+                    </div>
+                </div>
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselCabecera"
+                    data-bs-slide="prev"
+                >
+                    <span
+                        className="carousel-control-prev-icon"
+                        aria-hidden="true"
+                    ></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselCabecera"
+                    data-bs-slide="next"
+                >
+                    <span
+                        className="carousel-control-next-icon"
+                        aria-hidden="true"
+                    ></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
+    );
+}
+
+export function Card({ imgSrc, title, text }) {
+    return (
+        <div className="card me-sm-5" style={{ width: "25rem" }}>
+            <img src={imgSrc} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{text}</p>
+                <a href="#" className="btn btn-primary me-1 me-sm-1">
+                    Comprar
+                </a>
+                <a href="#" className="btn btn-secondary">
+                    Detalles
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export function Galeria() {
+    const loremIpsum =
+        "Lore ipsum dolor sit amet, consectetur adipisicing elit.";
+    const cards = [
+        { imgSrc: "src/img/1.jpg", title: "iPhone", text: loremIpsum },
+        { imgSrc: "src/img/2.jpg", title: "Mac", text: loremIpsum },
+        { imgSrc: "src/img/3.jpg", title: "Canon", text: loremIpsum },
+        { imgSrc: "src/img/4.jpg", title: "Apple Watch", text: loremIpsum },
+        { imgSrc: "src/img/5.jpg", title: "Keyboard", text: loremIpsum },
+        { imgSrc: "src/img/6.jpg", title: "iPad", text: loremIpsum },
+        { imgSrc: "src/img/7.jpg", title: "Macbook", text: loremIpsum },
+        { imgSrc: "src/img/8.jpg", title: "Cameras", text: loremIpsum },
+    ];
+
+    return (
+        <div className="row ms-5 me-3 ms-sm-5 me-sm-3">
+            <h2
+                className="text-center mt-5 mt-sm-5"
+                style={{ fontSize: "48px" }}
+            >
+                Galería
+            </h2>
+            <hr className="mb-5 mb-sm-5 bg-secondary" />
+            {cards.map((card) => (
+                <div className="col-3 col-sm-3 mb-5 mb-sm-5">
+                    <Card {...card} />
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export function Footer() {
+    return (
+        <footer className="row ms-5 me-4">
+            <hr className="mb-5 mb-sm-5"/>
+            <h4 className="text-secondary col-6 col-sm-6" style={{fontWeight: "light"}}>IES Pazo da Merce</h4>
+            <a href="#inicio" className="col-6 col-sm-6 text-end text-sm-end">
+                Subir en página
+            </a>
+            <a href="#" className="link-primary col-1 col-sm-1">
+                Acerca de
+            </a>
+            <a href="#" className="link-primary col-1 col-sm-1">
+                Contacto
+            </a>
+        </footer>
     );
 }
