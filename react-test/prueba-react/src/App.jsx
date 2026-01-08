ff20e163d116bf254ebf98d60a25f88e7da25f21
XaquinAlves <xaquinalves@gmail.com>
2026-01-08 09:52:14 +0100

    Added imgs

--- react-test/prueba-react/src/App.jsx
+++ react-test/prueba-react/src/App.jsx
@@ -8,6 +8,18 @@ function App() {
     return (
         <>
             <Navigator />
+            <Carrousel />
+        </>
+    );
+}
+
+export default App;
+
+export function Example() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
             <div className="row justify-content-center justify-content-sm-center">
                 <a
                     href="https://vite.dev"
@@ -42,8 +54,6 @@ function App() {
         </>
     );
 }
-
-export default App;
 
 export function Navigator() {
     return (
@@ -121,6 +131,70 @@ export function Navigator() {
                     </button>
                 </form>
             </nav>
+        </div>
+    );
+}
+
+export function Carrousel() {
+    return (
+        <div id="carouselExampleIndicators" className="carousel slide">
+            <div className="carousel-indicators">
+                <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="0"
+                    className="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                ></button>
+                <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="1"
+                    aria-label="Slide 2"
+                ></button>
+                <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="2"
+                    aria-label="Slide 3"
+                ></button>
+            </div>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="img/s1.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="img/s2.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="img/s3.jpg" class="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev"
+            >
+                <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next"
+            >
+                <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+            </button>
         </div>
     );
 }
